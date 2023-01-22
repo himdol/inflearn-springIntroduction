9ae0398ac0c9ecc384337ca2f61e93ee75e061d0
himdol <ihimchan746@gmail.com>
2023-01-22 17:50:41 +0900

    3강 라이브러리확인

---
+++ src/main/java/com/example/infleanintroduction/InflearnIntroductionApplication.java
@@ -0,0 +1,13 @@
+package com.example.infleanintroduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnIntroductionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InflearnIntroductionApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/infleanintroduction/InfleanIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.infleanintroduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfleanIntroductionApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
