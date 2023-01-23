7d5d6d7692134f9918cd56ade3e84b5f8a63810d
himdol <ihimchan746@gmail.com>
2023-01-23 15:00:39 +0900

    회원 service 구현

---
+++ src/main/java/com/example/infleanintroduction/service/MemberService.java
@@ -0,0 +1,38 @@
+package com.example.infleanintroduction.service;
+
+import com.example.infleanintroduction.domain.Member;
+import com.example.infleanintroduction.repository.MemberRepository;
+import com.example.infleanintroduction.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+  private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+  /***
+   * 회원가입
+   */
+
+  public Long join(Member member) {
+    validateDuplicateMember(member);
+    memberRepository.save(member);
+    return member.getId();
+  }
+
+  private void validateDuplicateMember(Member member) {
+    memberRepository.findByName(member.getName())
+              .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+              });
+  }
+
+  public List<Member> findMembers() {
+    return memberRepository.findAll();
+  }
+
+  public Optional<Member> findOne(Long memberId) {
+    return memberRepository.findById(memberId);
+  }
+}
