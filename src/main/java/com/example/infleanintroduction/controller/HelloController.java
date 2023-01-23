6d098d7660fb2b31d1882189cdda57843eb565bb
himdol <ihimchan746@gmail.com>
2023-01-23 13:22:34 +0900

    mvc 동적 spring 구현

--- src/main/java/com/example/infleanintroduction/controller/HelloController.java
+++ src/main/java/com/example/infleanintroduction/controller/HelloController.java
@@ -2,7 +2,9 @@ package com.example.infleanintroduction.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -19,4 +21,10 @@ public class HelloController {
   spring 이 실행이 되는 것을 알 수가 있다.
   컨트롤 + c 를 이용하여 종료할 수 있다.
   * */
+
+  @GetMapping("hello-mvc")
+  public String helloMvc(@RequestParam("name") String name, Model model) {
+    model.addAttribute("name", name);
+    return "hello-template";
+  }
 }
