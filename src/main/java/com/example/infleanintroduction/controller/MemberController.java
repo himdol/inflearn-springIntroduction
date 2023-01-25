cf586bf20b6747a2da70aec368168a21fb85bfa1
himdol <ihimchan746@gmail.com>
2023-01-26 07:16:02 +0900

    회원 가입 기능 구현 완료

--- src/main/java/com/example/infleanintroduction/controller/MemberController.java
+++ src/main/java/com/example/infleanintroduction/controller/MemberController.java
@@ -4,8 +4,11 @@ import com.example.infleanintroduction.domain.Member;
 import com.example.infleanintroduction.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -33,6 +36,18 @@ public class MemberController {
   public String create(MemberForm memberForm) {
     Member member = new Member();
     member.setName(memberForm.getName());
+
+    System.out.println(member.getName());
+
+    memberservice.join(member);
+
     return "redirect:/";
+  }
+
+  @GetMapping("/members")
+  public String list(Model model) {
+    List<Member> members = memberservice.findMembers();
+    model.addAttribute("members", members);
+    return "members/memberList";
   }
 }
