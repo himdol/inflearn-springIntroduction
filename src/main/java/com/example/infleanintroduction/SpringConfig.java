b0608a4b61fa57dee22968fd5b1093c76f3a7d4a
himdol <ihimchan746@gmail.com>
2023-01-28 17:45:08 +0900

    순수 jdbc 연결 성공

--- src/main/java/com/example/infleanintroduction/SpringConfig.java
+++ src/main/java/com/example/infleanintroduction/SpringConfig.java
@@ -1,16 +1,27 @@
 package com.example.infleanintroduction;
 
+import com.example.infleanintroduction.repository.JdbcMemberRepository;
 import com.example.infleanintroduction.repository.MemberRepository;
 import com.example.infleanintroduction.repository.MemoryMemberRepository;
 import com.example.infleanintroduction.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
   /*
   * 수동 Bean 생성이다. 참고
   * */
+  private DataSource dataSource;
+
+  public SpringConfig(DataSource dataSource) {
+    this.dataSource = dataSource;
+  }
+
   @Bean
   public MemberService memberService() {
     return new MemberService(memberRepository());
@@ -18,6 +29,7 @@ public class SpringConfig {
 
   @Bean
   public MemberRepository memberRepository() {
-    return new MemoryMemberRepository();
+//    return new MemoryMemberRepository();
+    return new JdbcMemberRepository(dataSource);
   }
 }
