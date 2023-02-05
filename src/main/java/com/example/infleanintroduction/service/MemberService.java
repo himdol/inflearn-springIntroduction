f0ae57a77baa21887d6ff96893e1b3e30c8d0557
himdol <ihimchan746@gmail.com>
2023-02-05 10:51:15 +0900

    AOP가 필요한 상황

--- src/main/java/com/example/infleanintroduction/service/MemberService.java
+++ src/main/java/com/example/infleanintroduction/service/MemberService.java
@@ -31,9 +31,19 @@ public class MemberService {
    * 회원가입
    */
   public Long join(Member member) {
-    validateDuplicateMember(member);
-    memberRepository.save(member);
-    return member.getId();
+
+    long start = System.currentTimeMillis();
+
+
+    try {
+      validateDuplicateMember(member);
+      memberRepository.save(member);
+      return member.getId();
+    } finally {
+      long finish = System.currentTimeMillis();
+      long timeMs = finish - start;
+      System.out.println("join = " + timeMs);
+    }
   }
 
   private void validateDuplicateMember(Member member) {
